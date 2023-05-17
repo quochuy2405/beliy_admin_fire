26ce0455e404fb6d4a8f36985e81113e8adc987e
quochuy2405 <huy01682312920@gmail.com>
2023-05-17 14:15:56 +0700

    update

--- components/templates/Login.tsx
+++ components/templates/Login.tsx
@@ -1,6 +1,4 @@
 "use client";
-import Link from "next/link";
-import React from "react";
 import { Controller } from "react-hook-form";
 const Login = ({ methods, handleSubmit }) => {
 	return (
--- utils/logint.ts
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup';
-export const loginSchema = yup.object().shape({
-    email: yup.string().email("Email không hợp lệ")
-        .required('Nhập email'),
-    password: yup
-        .string()
-        .min(8, 'Mật khẩu phải ít nhất 8 ký tự')
-        .required('Nhập mật khẩu')
-})
